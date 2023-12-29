1b2085469e89994cb591b7a86f5f45315280cb35
drduhe <drduhe@amazon.com>
2023-12-29 19:17:37 +0000

    fix: fixing model bugs

--- src/aws/osml/models/flood/app.py
+++ src/aws/osml/models/flood/app.py
@@ -3,6 +3,7 @@ import json
 import logging
 import math
 import os
+import random
 from random import randrange
 from secrets import token_hex
 from typing import Dict, Union
@@ -18,7 +19,8 @@ app.logger.setLevel(logging.ERROR)
 
 # Optional ENV configurations
 BBOX_PERCENTAGE = float(os.environ.get("BBOX_PERCENTAGE", 0.1))
-FLOOD_VOLUME = int(os.environ.get("FLOOD_VOLUME", 100))
+FLOOD_VOLUME = int(os.environ.get("FLOOD_VOLUME", 500))
+ENABLE_SEGMENTATION = os.environ.get("ENABLE_SEGMENTATION", "False").lower() == "true"
 
 
 def gen_flood_detects(height: int, width: int, bbox_percentage: float) -> Dict[str, Union[str, list]]:
@@ -42,13 +44,17 @@ def gen_flood_detects(height: int, width: int, bbox_percentage: float) -> Dict[s
             gen_x + fixed_object_size_xy[0],
             gen_y + fixed_object_size_xy[1],
         ]
-        fixed_object_mask = [
-            [gen_x - fixed_object_size_xy[0], gen_y + fixed_object_size_xy[1]],
-            [gen_y - fixed_object_size_xy[0], gen_x + fixed_object_size_xy[0]],
-            [gen_x + fixed_object_size_xy[0], gen_y + fixed_object_size_xy[1]],
-            [gen_y + fixed_object_size_xy[1], gen_x + fixed_object_size_xy[0]],
-        ]
-        feature = detect_to_feature(fixed_object_bbox, fixed_object_mask)
+        fixed_object_mask = None
+        if ENABLE_SEGMENTATION:
+            fixed_object_mask = [
+                [gen_x - fixed_object_size_xy[0], gen_y + fixed_object_size_xy[1]],
+                [gen_y - fixed_object_size_xy[0], gen_x + fixed_object_size_xy[0]],
+                [gen_x + fixed_object_size_xy[0], gen_y + fixed_object_size_xy[1]],
+                [gen_y + fixed_object_size_xy[1], gen_x + fixed_object_size_xy[0]],
+                [gen_x - fixed_object_size_xy[0], gen_y + fixed_object_size_xy[1]],
+            ]
+        # Create a feature with a random confidence score for each random detect
+        feature = detect_to_feature(fixed_object_bbox, fixed_object_mask, random.uniform(0, 1))
         geojson_features.append(feature)
 
     geojson_feature_collection_dict = {"type": "FeatureCollection", "features": geojson_features}
